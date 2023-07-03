0f9a784a162c6521d35ed2357878db424f33f39a
HugoJ0 <73164643+HugoJ0@users.noreply.github.com>
2023-07-03 16:29:59 +0200

    feat: add toString method to the SudokuGrid

--- src/main/scala/SudokuGrid.scala
+++ src/main/scala/SudokuGrid.scala
@@ -1,3 +1,5 @@
+package sudoku
+
 /**
   * Represents a Sudoku grid.
   *
@@ -6,3 +8,20 @@
   *              The outer vector represents the entire grid. 
   */
 case class SudokuGrid(cells: Vector[Vector[Int]])
+{
+    /**
+      * Returns a string representation of the Sudoku grid.
+      * The grid is formatted with separators for lines and sub-grids.
+      * Empty cells (with value 0) are represented as spaces.
+      *
+      * @return A string representing the Sudoku grid.
+      */
+    override def toString: String = {
+        val separator = "+-------+-------+-------+\n"
+        cells.zipWithIndex.map { case (row, rowIndex) =>
+          val formattedRow = row.map(cellValue => if (cellValue == 0) " " else cellValue.toString).grouped(3).map(_.mkString(" ")).mkString(" | ")
+          val rowSeparator = if (rowIndex % 3 == 2) separator else ""
+          if (rowIndex == 0) s"$separator| $formattedRow |\n" else s"| $formattedRow |\n$rowSeparator"
+        }.grouped(3).map(_.mkString).mkString
+    }
+}
